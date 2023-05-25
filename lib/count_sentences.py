7cf5db48f24da4472e1906241fbc2d8cece13fb5
richard miruka onsare <richardonsare15@gmail.com>
2023-05-25 17:16:47 +0300

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
-
+import re 
 class MyString:
-  pass
+  def __init__(self,value = None):
+      self.value = value
+  @property
+  def value(self):
+    return self._value
+  @value.setter
+  def value(self, new_value):
+    if new_value is not None and not isinstance(new_value, str):
+            print("The value must be a string.")
+    else:
+            self._value = new_value
+   
+  def is_sentence(self):
+        if self._value is not None and self._value.endswith('.'):
+            return True
+        return False         
+        
+  def is_question(self):
+        if self._value is not None and self._value.endswith('?'):
+            return True
+        return False
+      
+  def is_exclamation(self):
+        if self._value is not None and self._value.endswith('!'):
+            return True
+        return False
+  
+  def count_sentences(self):
+      if not self.value:
+        return 0
+      sentences = [sentence for sentence in re.split(r'[.!?]', self.value) if sentence]
+      return len(sentences)
+    
